146c1558620c6fa6cc5b465a7fe98704ead07335
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-08 17:54:34 -0300

    feat: css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Post } from './Post'
+import { Header } from './components/Header'
+
+import './styles.css'
 
 export function App() {
   return (
     <>
+      <Header />
       <Post 
         author="Eduardo Diniz" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aut omnis eveniet animi, distinctio velit quibusdam quisquam, commodi id quidem vero quae. Sapiente assumenda earum consequatur expedita, ut numquam saepe?" 
