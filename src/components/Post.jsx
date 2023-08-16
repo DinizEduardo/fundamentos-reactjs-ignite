2ac29fae4d9dd8ef1dd2512b5273e8bf32492d48
Eduardo Diniz <eduardo.diniz987@gmail.com>
2023-08-16 16:38:46 -0300

    feat: formulario de comentarios

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -24,6 +24,20 @@ export function Post() {
           <a href="">#nlw</a>{' '}
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder='Deixe um comentario'
+        />
+
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+        
+
+      </form>
     </article>
   )
 }
